af11873a8ecfc0f4ee6eeae4547c6d8b277dfbd3
Kerri L OGorman <kerri.l.ogorman@gmail.com>
2020-08-10 18:54:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,6 +7,14 @@ class Song
     @name = name
   end
 
+  def save
+   @@all << self
+  end
+
+  def self.all #setting a getter method so when a song is initialized, we can push the instance into the @@all an dbe able to ue Song.all to retrieve all Song instances
+   @@all
+  end
+
   def artist_name
    self.artist.name
   end
