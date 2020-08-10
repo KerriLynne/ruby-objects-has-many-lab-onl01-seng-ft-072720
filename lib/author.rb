6ef3c57d9b9a3af65f9e65e0654dd273aafee406
Kerri L OGorman <kerri.l.ogorman@gmail.com>
2020-08-10 19:36:51 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -6,20 +6,20 @@ class Author
   def initialize(name)
     @name = name
   end
-  
+
   def posts
     Post.all.select {|post| post.author == self}
   end
 
   def add_post(post)
     post.author = self
-  
+
   end
 
   def add_post_by_title(title)
     post = Post.new(title)
     post.author = self
-  
+
   end
 
   # def self.song_count
